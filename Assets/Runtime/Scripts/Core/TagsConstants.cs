ed9fc0f715f0616ffa1129320be33f655ba6d8f7
Leonardo Feitosa <leonardo.feitosa@ambrait.com>
2022-11-15 21:44:37 -0300

    add tag explosions

--- Assets/Runtime/Scripts/Core/TagsConstants.cs
+++ Assets/Runtime/Scripts/Core/TagsConstants.cs
@@ -5,4 +5,5 @@ public class TagsConstants
     public const string LimitsClosePassage = "LimitsClosePassage";
     public const string Grease = "Grease";
     public const string Finish = "Finish";
+    public const string Explosions = "Explosions";
 }
